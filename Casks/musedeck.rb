1788272892a0d221717f355ee54fff70a55cb2d4
ashutoshb206 <ashutoshbansalcs@gmail.com>
2026-08-05 23:58:33 +0530

    checksum: update MuseDeck.dmg SHA256 for status menu redesign

--- Casks/musedeck.rb
+++ Casks/musedeck.rb
@@ -1,6 +1,6 @@
 cask "musedeck" do
   version "1.2.0"
-  sha256 "9bcc87f746bf2f6a5e4572b1a1166d4883d950a84ce226aa4039eec8cd5cad42"
+  sha256 "a6623daed485215d8d55d4cf4ffd043ed679fa67ed53686466fc94f2507c5127"
 
   url "https://muse-deck.vercel.app/MuseDeck.dmg"
   name "MuseDeck"
