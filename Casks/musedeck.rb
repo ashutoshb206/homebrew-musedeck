e9789a82a906b940ba9e0e3e84616f3aa3a50a8f
ashutoshb206 <ashutoshbansalcs@gmail.com>
2026-08-05 23:18:57 +0530

    url: update Cask download URL for MuseDeck.dmg

--- Casks/musedeck.rb
+++ Casks/musedeck.rb
@@ -2,17 +2,15 @@ cask "musedeck" do
   version "1.2.0"
   sha256 "cb8112ca1e2d6ab8ac4117b20fc6708dfd0a66d54aea5d1a5f32124eb1e38b1b"
 
-  url "https://github.com/ashutoshb206/MuseDeck/releases/download/v#{version}/MuseDeck.dmg"
+  url "https://muse-deck.vercel.app/MuseDeck.dmg"
   name "MuseDeck"
-  desc "Floating liquid-glass desktop music widget with time-synced lyrics & vinyl deck"
-  homepage "https://github.com/ashutoshb206/MuseDeck"
-
-  depends_on macos: ">= :sonoma"
+  desc "Floating liquid-glass desktop widget for your music"
+  homepage "https://muse-deck.vercel.app/"
 
   app "MuseDeck.app"
 
   zap trash: [
-    "~/Library/Application Support/com.ashutoshbansal.musedeck",
+    "~/Library/Application Support/MuseDeck",
     "~/Library/Preferences/com.ashutoshbansal.musedeck.plist",
   ]
 end
