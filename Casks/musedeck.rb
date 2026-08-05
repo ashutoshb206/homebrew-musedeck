dab79106d4f0b742a7ffbecf05cae5d889baa27a
ashutoshb206 <ashutoshbansalcs@gmail.com>
2026-08-05 21:28:05 +0530

    Update MuseDeck.dmg SHA256 checksum for desktop window isolation release

--- Casks/musedeck.rb
+++ Casks/musedeck.rb
@@ -1,6 +1,6 @@
 cask "musedeck" do
   version "1.2.0"
-  sha256 "15718b6c27f09ad10ab109d5d797695a165c1f63ab417df6e97ee474286b2c20"
+  sha256 "cc19d89234548aaefd3bd8e08d327814e4ef0bbcc60072926b444e1031313a95"
 
   url "https://github.com/ashutoshb206/MuseDeck/releases/download/v#{version}/MuseDeck.dmg"
   name "MuseDeck"
