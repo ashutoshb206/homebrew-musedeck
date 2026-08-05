7958f557e5b06b9c56d48be9c22c5967da0cbf2a
ashutoshb206 <ashutoshbansalcs@gmail.com>
2026-08-05 23:52:22 +0530

    checksum: update MuseDeck.dmg SHA256

--- Casks/musedeck.rb
+++ Casks/musedeck.rb
@@ -1,6 +1,6 @@
 cask "musedeck" do
   version "1.2.0"
-  sha256 "cb8112ca1e2d6ab8ac4117b20fc6708dfd0a66d54aea5d1a5f32124eb1e38b1b"
+  sha256 "9bcc87f746bf2f6a5e4572b1a1166d4883d950a84ce226aa4039eec8cd5cad42"
 
   url "https://muse-deck.vercel.app/MuseDeck.dmg"
   name "MuseDeck"
