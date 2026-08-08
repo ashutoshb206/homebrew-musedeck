343081c2284550010bb7199dba6c64aa11e98271
ashutoshb206 <ashutoshbansalcs@gmail.com>
2026-08-08 14:14:28 +0530

    checksum: update SHA256 and version to v1.2.3 — Fix BuyMeACoffee link & Gatekeeper card

--- Casks/musedeck.rb
+++ Casks/musedeck.rb
@@ -1,6 +1,6 @@
 cask "musedeck" do
-  version "1.2.2"
-  sha256 "c9f56fe94868529a299467ec98f3ef3fe9c98a3e1ef9addd13d9d25d8a8d562b"
+  version "1.2.3"
+  sha256 "cf10161e7a238eaca64de410c04d38dfc09f59b855e8067b03273677fef7f0cb"
 
   url "https://muse-deck.vercel.app/MuseDeck.dmg"
   name "MuseDeck"
